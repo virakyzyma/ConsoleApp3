27a63efd69aee43a0c4ac9fa70618da68ee5727b
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-01-25 13:00:10 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace First_HW
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number ending with 5: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int NewNumber = 0;
+            if (number % 10 == 5)
+            {
+                NewNumber = ((number / 10) * ((number / 10) + 1) * 100 + 25);
+            }
+            else
+            {
+                Console.WriteLine("The number does not end with 5");
+            }
+            Console.WriteLine($"Result: {NewNumber}");
+        }
+    }
+}
